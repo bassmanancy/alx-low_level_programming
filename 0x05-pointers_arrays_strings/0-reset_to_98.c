432998a0285f744cb9b05fabf2af2346a4a3a524
Your Name <you@example.com>
2023-10-17 14:34:15 -0700

    s

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,4 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Bard
- *
- * Desc: Updates the value pointed to by the parameter to 98.
- */
-
 #include "main.h"
-
 /**
  * reset_to_98 - Updates the value pointed to by n to 98.
  * @n: A pointer to an int.
