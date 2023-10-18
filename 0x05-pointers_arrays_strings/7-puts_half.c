2c722d9563dd2672bd6f37fc9e79c7398aa1a77c
Your Name <you@example.com>
2023-10-17 17:05:22 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string, followed by a new line.
+ * @str: A pointer to the string.
+ * Return: Always 0.
+ */
+
+/*
+ * File: 7-puts_half.c
+ * Auth: Bard
+ *
+ * Desc: Prints half of a string, followed by a new line.
+ * @str: A pointer to the string.
+ *
+ * Return: Nothing.
+ */
+
+void puts_half(char *str)
+{
+	char *start = str;
+	char *end = str + _strlen(str) - 1;
+
+	if (_strlen(str) % 2 == 0)
+	{
+		start += _strlen(str) / 2;
+	}
+	else
+	{
+		start += (_strlen(str) - 1) / 2 + 1;
+	}
+
+	while (*start != '\0')
+	{
+		_putchar(*start);
+		start++;
+	}
+
+	_putchar('\n');
+}
