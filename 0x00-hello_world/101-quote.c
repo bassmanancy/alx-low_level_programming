956b6513ef7cd9da1e02100dd6d0d4062375c162
Your Name <you@example.com>
2023-10-19 07:02:44 -0700

    S

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -8,7 +9,7 @@ int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, quote, strlen(quote));
+	fwrite(STDERR_FILENO, quote, strlen(quote));
 
 	return (1);
 }
