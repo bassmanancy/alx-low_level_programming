b226d938700ad817d0d2ddcb9ce9daa85f53266c
Your Name <you@example.com>
2023-10-19 07:10:36 -0700

    S

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * main - Entry point
  *
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	
 	return (1);
 }
