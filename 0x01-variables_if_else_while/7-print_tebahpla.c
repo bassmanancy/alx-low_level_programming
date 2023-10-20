c56357fe1bd0818251125b016917ff98fbfbd6c5
Your Name <you@example.com>
2023-10-20 08:37:16 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar (c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
