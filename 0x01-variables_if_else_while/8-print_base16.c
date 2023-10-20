ed8edc78b394bbc1eb6cdcfc55cadf0901ecf954
Your Name <you@example.com>
2023-10-20 08:50:22 -0700

    k

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int = i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else if (i >= 10)
+		{
+			putchar (i + 'a' - 10);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
