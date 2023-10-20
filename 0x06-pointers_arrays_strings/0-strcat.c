4d2c6583532d003f4744ba646f73334da9e2bef6
Your Name <you@example.com>
2023-10-20 14:15:03 -0700

    g

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,20 +4,22 @@
  * _strcat - Concatenates two strings.
  * @dest: A concatenated upon string.
  * @src: Appended string to @dest.
- *
  * Return: Destination string @ dest.
  */
 
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest,  char *src)
 {
 	int index = 0;
-       	int dest_len = 0;
+	int dest_len = 0;
+	int i = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (src[index] != '\0')
+		index++;
+	while (dest[dest_len} != '\0')
+	dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (i = dest_len; i <= (deset_len + index); i++)
+		dest[i] = src[i - dest_len];
 
 	return (dest);
 }
