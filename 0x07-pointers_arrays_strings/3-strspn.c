37e4720691364edb0a364bddcef7983ecd7113c4
Your Name <you@example.com>
2023-10-24 13:28:29 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int bytes = 0;
+	int i;
 
-	i = 0;
-	while (*s && _strchr(accept, *s))
+	hile (*s)
 	{
-		i++;
+		for (i = 0; accept [i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (bytes);
+		}
+
 		s++;
 	}
-	return (i);
+
+	return (bytes);
 }
