f6ef4c6f60ae368013d918e1dec00858f22ec3d7
Your Name <you@example.com>
2023-10-24 13:01:51 -0700

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  * @src: memory area to copy characters from.
  * Return: dest.
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
